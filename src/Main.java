77bb0d61c026d77b4ea60e8c6d2858840d7691e8
dpanchugin <d.panchugin@bk.ru>
2024-11-29 13:33:05 +0300

    add arifmetic operation

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма");
+        System.out.println(number1+number2);
+        System.out.println("Разность Первое число - Второе число");
+        System.out.println(number1-number2);
+        System.out.println("Умножение двух чисел");
+        System.out.println(number1*number2);
+        System.out.println("Деление первого числа на второе число");
+        System.out.println((double) number1/number2);
+
     }
 
 }
