5e309afc6acdb896a5fe8ce784c7e33ec072d63a
dpanchugin <d.panchugin@bk.ru>
2024-12-12 11:51:08 +0300

    Проверка директории

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
-    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int attemptCount = 0; // Счётчик попыток ввода
+
+        while (true) {
+            System.out.printf("Введите путь к файлу: ", ++attemptCount);
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Файла или директории не существует.");
+                continue;
+            }
+
+            if (isDirectory) {
+                System.out.println("Указан путь к директории, а не к файлу.");
+                continue;
+            }
+
+            System.out.println("Путь к файлу указан верно. Количество затраченных попыток: " + attemptCount);
+            break; // Если файл найден, выходим из цикла
+        }
+    }
 }
